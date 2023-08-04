d681d09e90cdfa5dbf1ddfedc3e616e093f17def
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-03 21:03:02 -0300

    initial commit

--- src/Game.jsx
+++ src/Game.jsx
@@ -19,7 +19,7 @@ export default function Game(){
 
     const moves = history.map((squares, move)=>{
         let description;
-        if(move > 0){
+        if(move>0){
             description = 'Go to move #' + move;
         } else {
             description = 'Go to game start';
